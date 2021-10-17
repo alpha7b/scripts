da5eed42e0524ad00afea50ca8cce11f17c480f6
alpha7b <55778604+alpha7b@users.noreply.github.com>
2021-10-17 11:28:22 +0800

    Update massaSetup.sh

--- massaSetup.sh
+++ massaSetup.sh
@@ -1,9 +1,9 @@
-# curl -sL https://raw.githubusercontent.com/alpha7bet/scripts/main/massaSetup.sh -o massaSetup.sh && sudo bash massaSetup.sh
+# curl -sL https://raw.githubusercontent.com/alpha7/scripts/main/massaSetup.sh -o massaSetup.sh && sudo bash massaSetup.sh
 
 # setup massa testnet node
 # https://gitlab.com/massalabs/massa/-/tree/testnet
 
-
+sudo apt update
 sudo apt install pkg-config curl git build-essential libssl-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.cargo/env
@@ -15,4 +15,3 @@ rustc --version
 git clone --branch testnet https://gitlab.com/massalabs/massa.git
 cd massa/massa-node/
 RUST_BACKTRACE=full cargo run --release |& tee logs.txt
-
