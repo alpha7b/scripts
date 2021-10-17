4af72e66c9cff6e7ec1cd7e791f14528432a68e2
alpha7b <55778604+alpha7b@users.noreply.github.com>
2021-10-17 11:26:53 +0800

    Create massaSetup.sh

---
+++ massaSetup.sh
@@ -0,0 +1,18 @@
+# curl -sL https://raw.githubusercontent.com/alpha7bet/scripts/main/massaSetup.sh -o massaSetup.sh && sudo bash massaSetup.sh
+
+# setup massa testnet node
+# https://gitlab.com/massalabs/massa/-/tree/testnet
+
+
+sudo apt install pkg-config curl git build-essential libssl-dev
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+rustc --version
+rustup toolchain install nightly
+rustup default nightly
+rustc --version
+
+git clone --branch testnet https://gitlab.com/massalabs/massa.git
+cd massa/massa-node/
+RUST_BACKTRACE=full cargo run --release |& tee logs.txt
+
