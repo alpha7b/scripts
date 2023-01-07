2511c9d3c6914053ee4e9200f374b0694cdc723c
alpha7b <55778604+alpha7b@users.noreply.github.com>
2023-01-08 01:00:40 +0800

    Update massaSetup.sh

--- massaSetup.sh
+++ massaSetup.sh
@@ -1,5 +1,4 @@
 # curl -sL https://raw.githubusercontent.com/alpha7b/scripts/main/massaSetup.sh -o massaSetup.sh && sudo bash massaSetup.sh
-
 # setup massa testnet node
 # https://docs.massa.net/en/latest/testnet/install.html
 
@@ -11,18 +10,28 @@ screen -S massa -X quit
 echo "Display all screen sessions"
 screen -ls
 
+sleep 5s
+
+# set version
+read -p "Enter Massa Version: " MassaVersion
+echo $MassaVersion
 
 cd ~/massa
 rm -rf massa_TEST.*
-wget https://github.com/massalabs/massa/releases/download/TEST.18.0/massa_TEST.18.0_release_linux.tar.gz
-tar -xvf massa_TEST.18.0_release_linux.tar.gz
+wget https://github.com/massalabs/massa/releases/download/TEST.$MassaVersion/massa_TEST.$MassaVersion'_release_linux.tar.gz'
+tar -xvf massa_TEST.$MassaVersion'_release_linux.tar.gz'
 pwd
+ls
+
+sleep 5s
 
 # add these info to ~/massa/massa-node/base_config/config.toml
 # [network]
 # routable_ip = "xxx.xxx.xxx.xxx"
 sed -i "/\[network\]/a\    routable_ip = \"$(curl -s ifconfig.me)\"" ~/massa/massa/massa-node/base_config/config.toml
 cat ~/massa/massa/massa-node/base_config/config.toml
+
+sleep 5s
 
 # start node in screen session
 screen -dmS "massa" bash -c "
@@ -32,5 +41,6 @@ cd ~/massa/massa/massa-node/;
 pwd; 
 ls;
 ./massa-node -p 123 |& tee logs.txt;
+sleep 40s;
 bash
 "
