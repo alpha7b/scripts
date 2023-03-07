1e3d0676e084427cd797a5ef2cafef52a4597934
alpha7b <55778604+alpha7b@users.noreply.github.com>
2023-03-07 10:54:32 +0800

    Update claim_nibiru_faucet.sh

--- claim_nibiru_faucet.sh
+++ claim_nibiru_faucet.sh
@@ -1,4 +1,3 @@
-# curl -sL https://raw.githubusercontent.com/alpha7b/scripts/main/claim_nibiru_faucet.sh -o claim_nibiru_faucet.sh && sudo bash claim_nibiru_faucet.sh
 FAUCET_URL="https://faucet.itn-1.nibiru.fi/"
 ADDR=$(echo $key | nibid keys show wallet -a)
 curl -X POST -d '{"address": "'"$ADDR"'", "coins": ["11000000unibi","100000000unusd","100000000uusdt"]}' $FAUCET_URL
