725fd17b314ce2e0bd001751ffddbdfdc1b319e5
alpha7b <55778604+alpha7b@users.noreply.github.com>
2023-03-12 11:23:50 +0800

    $hoprdImage

--- hoprd_restart.sh
+++ hoprd_restart.sh
@@ -3,8 +3,8 @@
 export ipAddr=$(curl ifconfig.me)
 echo "ipAddr is: "
 echo $ipAddr
-export hoprImage="gcr.io/hoprassociation/hoprd:1.92.9"
-echo $hoprImage
+export hoprdImage="gcr.io/hoprassociation/hoprd:1.92.9"
+echo $hoprdImage
 
 # terminate hoprd screen session
 function terminate_existing_hoprd_screen_session(){
@@ -22,12 +22,12 @@ function stop_running_hoprd_container(){
     echo "Display all hoprd containers"
     docker ps -a --filter ancestor=$hoprImage
     echo "stop running hoprd container"
-    docker stop $(docker ps -a -q --filter ancestor=$hoprImage --filter status=running)
+    docker stop $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=running)
     sleep 20s
     echo "remove exited hoprd container"
-    docker rm $(docker ps -a -q --filter ancestor=$hoprImage --filter status=exited)
+    docker rm $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=exited)
     echo "Display all hoprd containers"
-    docker ps -a --filter ancestor=$hoprImage
+    docker ps -a --filter ancestor=$hoprdImage
     sleep 5s
 }
 
@@ -43,7 +43,7 @@ function start_hoprd(){
         cd cd ~/hopr; 
         pwd; 
         ls;
-        docker run --pull always --restart on-failure -m 2g --log-driver json-file --log-opt max-size=100M --log-opt max-file=5 -ti -v $HOME/.hoprd-db-monte-rosa:/app/hoprd-db -p 9091:9091/tcp -p 9091:9091/udp -p 8080:8080 -p 3001:3001 -e DEBUG="hopr*" gcr.io/hoprassociation/hoprd:1.92.9 --environment monte_rosa --init --api --identity /app/hoprd-db/.hopr-id-monte-rosa --data /app/hoprd-db --password 'open-sesame-iTwnsPNg0hpagP+o6T0KOwiH9RQ0' --apiHost "0.0.0.0" --apiToken $apiToken --healthCheck --healthCheckHost "0.0.0.0";
+        docker run --pull always --restart on-failure -m 2g --log-driver json-file --log-opt max-size=100M --log-opt max-file=5 -ti -v $HOME/.hoprd-db-monte-rosa:/app/hoprd-db -p 9091:9091/tcp -p 9091:9091/udp -p 8080:8080 -p 3001:3001 -e DEBUG="hopr*" $hoprdImage --environment monte_rosa --init --api --identity /app/hoprd-db/.hopr-id-monte-rosa --data /app/hoprd-db --password 'open-sesame-iTwnsPNg0hpagP+o6T0KOwiH9RQ0' --apiHost "0.0.0.0" --apiToken $apiToken --healthCheck --healthCheckHost "0.0.0.0";
         sleep 40s;
         bash
     "
@@ -54,10 +54,10 @@ function remove_old_hoprd_container(){
     sleep 30s
     docker ps -a
     echo 'Remove exited hoprd container'
-    docker rm $(docker ps -a -q --filter ancestor=gcr.io/hoprassociation/hoprd:1.92.9 --filter status=exited)
+    docker rm $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=exited)
     sleep 30s
     echo 'Remove created hoprd container'
-    docker rm $(docker ps -a -q --filter ancestor=gcr.io/hoprassociation/hoprd:1.92.9 --filter status=created)
+    docker rm $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=created)
     docker ps -a
 }
 
