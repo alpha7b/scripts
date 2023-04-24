f745f2bf197f0ae1c44cf1e8b9fcd3a5d49f8f0b
alpha7b <55778604+alpha7b@users.noreply.github.com>
2023-04-24 21:46:41 +0800

    kill hoprd containers

--- hoprd_restart.sh
+++ hoprd_restart.sh
@@ -21,11 +21,12 @@ function stop_running_hoprd_container(){
     echo "Display all hoprd containers"
     docker ps -a --filter ancestor=$hoprImage
     echo "stop running hoprd container"
-    docker kill $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=running)
-    docker stop $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=running)
+    docker kill $(docker ps -a -q --filter ancestor=$hoprdImage)
+#     docker kill $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=running)
     sleep 20s
     echo "remove exited hoprd container"
-    docker rm $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=exited)
+    docker rm $(docker ps -a -q --filter ancestor=$hoprdImage)
+#     docker rm $(docker ps -a -q --filter ancestor=$hoprdImage --filter status=exited)
     echo "Display all hoprd containers"
     docker ps -a --filter ancestor=$hoprdImage
     sleep 5s
