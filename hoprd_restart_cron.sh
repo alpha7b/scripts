615422e626df8cdf68b0332b791154d8f91689ad
alpha7b <55778604+alpha7b@users.noreply.github.com>
2023-04-29 08:31:34 +0800

    complete command

--- hoprd_restart_cron.sh
+++ hoprd_restart_cron.sh
@@ -1,4 +1,4 @@
-curl -sL https://raw.githubusercontent.com/alpha7b/scripts/main/hoprd_restart_cron.sh -o ~/hopr/hoprd_restart_cron.sh && sudo bash hoprd_restart_cron.sh $apiToken
+# export apiToken="<apiToken>" && mkdir -p ~/hopr && curl -sL https://raw.githubusercontent.com/alpha7b/scripts/main/hoprd_restart_cron.sh -o ~/hopr/hoprd_restart_cron.sh && sudo bash ~/hopr/hoprd_restart_cron.sh $apiToken
 #!/bin/bash
 export hoprdImage="gcr.io/hoprassociation/hoprd:1.92.12"
 
