b5accadc0586003f7743169775569d4643661922
alpha7b <55778604+alpha7b@users.noreply.github.com>
2023-05-12 09:37:19 +0800

    Create dockerd_restart.sh

---
+++ dockerd_restart.sh
@@ -0,0 +1,8 @@
+# mkdir -p ~/dockerd_restart && curl -sL https://raw.githubusercontent.com/alpha7b/scripts/main/dockerd_restart.sh -o ~/dockerd_restart/dockerd_restart.sh && chmod +x ~/dockerd_restart/dockerd_restart.sh && sudo bash ~/dockerd_restart/dockerd_restart.sh
+#!/bin/bash
+
+# 杀死所有正在运行的docker容器
+ps axf | grep docker | grep -v grep | awk '{print "kill -9 " $1}' | sudo sh
+
+# 启动docker服务
+/usr/bin/dockerd -H unix://
